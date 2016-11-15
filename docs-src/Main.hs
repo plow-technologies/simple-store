fde5811557e04f907277e0348c4a8da9772d652e
Scott Murphy <scottmurphy09@gmail.com>
2016-11-15 17:22:41 -0600

    Added tools to dynamically find package and name info

--- docs-src/Main.hs
+++ docs-src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Main where
 
 import Development.Shake ( shakeArgs
@@ -13,15 +15,39 @@ import Development.Shake ( shakeArgs
                          , (%>)
                          , liftIO)
 import           Development.Shake.FilePath ((</>))
+import           Control.Monad (filterM)
+
+import qualified Filesystem
+--       (isDirectory, getModified, listDirectory, isFile, writeFile) 
+import qualified Filesystem.Path.CurrentOS as CurrentOS
+--       (FilePath, (</>),  fromText)
+import Data.Either (rights)
+import Data.Text (Text,unpack)
+import Control.Lens
+import Data.Aeson.Lens
+import Text.Regex.Lens
+import Text.Regex.Quote
+import Text.Regex.Posix (Regex)
+import qualified Data.Yaml as Yaml
+
+
+--------------------------------------------------
+-- Types
+--------------------------------------------------
+
+
+data GeneratedStaticRules = GeneratedStaticRules {
+                                  generatedWants :: [FilePath],
+                                  generatedRules :: [Rules ()] }
 
 
 
-
-
-
-
-
-
+data NamesThatMustBeDiscovered = NamesThatMustBeDiscovered
+  {
+    cabalPath :: FilePath,
+      ltsPath :: FilePath,
+  packageName :: String
+  } deriving (Eq,Show,Ord)
 
 
 
@@ -30,7 +56,7 @@ import           Development.Shake.FilePath ((</>))
 
 
 main :: IO ()
-main = shakeArgs shakeOptions { shakeFiles = buildDir
+main = shakeArgs shakeOptions { shakeFiles    = buildDir
                               , shakeProgress = progressSimple}
                  buildTheDocsRules
   where
@@ -41,19 +67,33 @@ main = shakeArgs shakeOptions { shakeFiles = buildDir
 --------------------------------------------------
 -- Rules
 --------------------------------------------------
-
+-- | Top level of document generation
 buildTheDocsRules :: Rules ()
 buildTheDocsRules = do
-  want [haddockInDocsIndex, haddockInStackWorkIndex]
-  stackHaddockRule
-  docsHaddockRule 
+  GeneratedStaticRules wants rules <- runDynamics
+  want wants
+  _ <- sequence rules
+  return ()
+
+
+
+
+-- | build elements that depend on specific configurations of stack.yaml
+--  this mostly involves setting up the correct cabal and lts directories
+--  for copying over the documentation. 
+runDynamics = do  
+  return $ GeneratedStaticRules [haddockInDocsIndex, haddockInStackWorkIndex] [ stackHaddockRule
+                                                                              , docsHaddockRule ]
 
 
 
 
 -- | Build the documentation in the .stack-work folder  
+
 stackHaddockRule :: Rules ()
-stackHaddockRule = haddockInStackWorkIndex %> \_ -> do
+stackHaddockRule = haddockInStackWorkIndex %> \out -> do
+  liftIO $ putStrLn "Poop"
+  liftIO $ print out
   stackHaddockCommand
 
 
@@ -61,9 +101,9 @@ stackHaddockRule = haddockInStackWorkIndex %> \_ -> do
 docsHaddockRule :: Rules ()
 docsHaddockRule = haddockInDocsIndex %> \_ -> do
     need [haddockInStackWorkIndex]
+    
     copyOtherPackagesCommand -- This needs to come before copyHaddock
     copyHaddockCommand
-
 
 
 
@@ -108,8 +148,10 @@ haddockInStackWork :: FilePath
 haddockInStackWork = ".stack-work" </> "dist" </>"x86_64-linux"</>"Cabal-1.22.5.0"</>"doc"</> "html" </> "simple-store"
 
 -- index.html for package docs
+haddockInStackWorkIndex :: FilePath
 haddockInStackWorkIndex = haddockInStackWork </> "index.html"
 
+haddockOtherPackagesInStackWork :: FilePath
 haddockOtherPackagesInStackWork = ".stack-work"</>"install"</>"x86_64-linux"</>"lts-6.13"</>"7.10.3"</>"doc"
 
 haddockInDocs :: FilePath
@@ -118,3 +160,50 @@ haddockInDocs = "docs"
 haddockInDocsIndex :: FilePath
 haddockInDocsIndex = haddockInDocs </> "index.html"
 
+
+stackWorkInstallPath :: CurrentOS.FilePath -> CurrentOS.FilePath
+stackWorkInstallPath wd = (wd CurrentOS.</> ".stack-work" CurrentOS.</> "install")
+
+--------------------------------------------------
+-- Dynamic Directory Lookup
+--------------------------------------------------
+
+
+getDirectories :: CurrentOS.FilePath -> IO [CurrentOS.FilePath]
+getDirectories wd = do
+  dirs <- Filesystem.listDirectory wd
+  filterM Filesystem.isDirectory dirs
+
+
+-- Get a full path target
+-- FilePath "<working-dir>/.stack-work/install/x86_64-linux"
+getTarget :: IO CurrentOS.FilePath
+getTarget = do
+ wd         <- Filesystem.getWorkingDirectory
+ (dir:_)    <- getDirectories $ stackWorkInstallPath wd
+ return dir
+
+
+
+buildNamesThatMustBeDiscovered = do
+                                target       <- getTarget
+                                eitherDirs   <- getDirectories target
+                                mPkgName      <- getPackageInfo
+                                let eitherTextDirs   = fmap unpack . CurrentOS.toText . CurrentOS.basename <$> eitherDirs 
+                                    maybeLtsString   = eitherTextDirs ^? folded . _Right . regex [r|lts.*|] . matchedString
+                                    maybeCabalString = eitherTextDirs ^? folded . _Right . regex [r|Cabal.*|] . matchedString
+                                                                                                 
+                                return $ NamesThatMustBeDiscovered <$> maybeLtsString  <*> maybeCabalString <*> mPkgName
+
+ex = "test" ^? regex [r|te|] . matchedString :: Maybe String
+
+
+
+--------------------------------------------------
+-- Package Info
+--------------------------------------------------
+
+getPackageInfo :: IO (Maybe String)
+getPackageInfo = do
+  pkg <- Yaml.decodeFileEither "package.yaml" :: IO (Either Yaml.ParseException Yaml.Value)
+  return $ (pkg ^? _Right . key "name" . _String <&> unpack)
